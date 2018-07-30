cf5dff9b1e784891ec35f56b03c8e0f53f7bacee
Claudia Cortes <clau_cortes10@hotmail.com>
2018-07-30 00:48:07 -0600

    Ordena

--- main.cpp
+++ main.cpp
@@ -24,6 +24,7 @@ bool Contains(vector<TreeElement *>, string);
 int IndexOf(vector<TreeElement *>, string);
 //vector<TreeElement *> Ordenar(vector<TreeElement *>);
 vector<TreeNode *> Ordenar(vector<TreeNode *>);
+vector<TreeNode *> poner(vector<TreeNode *>, TreeNode *);
 TreeNode *CreateTree(vector<TreeNode *>);
 vector<TreeNode *> Fill(vector<TreeElement *>);
 void CodeGenerator();
@@ -40,21 +41,37 @@ int main(int argc, char *argv[])
 		{
 			string path;
 			vector<TreeElement *> generalElements = ReadFile(path = argv[1]);
-			//generalElements = Ordenar(generalElements);
+			/****/
+			vector<TreeElement *> generalElements1;
+			for(int i=0;i<generalElements.size();i++){
+				if(generalElements.at(i)->GetElement()=="LF"){
+				
+				}else{
+					generalElements1.push_back(generalElements.at(i));
+				}
+			}
+			generalElements=generalElements1;
+			/****/
 			vector<TreeNode *> nodes = Fill(generalElements);
+
+			size_t tot;
 			for (int i = 0; i < nodes.size(); i++)
 			{
-				cout << "Letra: " << nodes.at(i)->GetData()->GetElement() << " " << nodes.at(i)->GetData()->GetFrequency()<<endl;
+				//cout << i << "Letra: " << nodes.at(i)->GetData()->GetElement() << " " << nodes.at(i)->GetData()->GetFrequency() << endl;
+				tot += nodes.at(i)->GetData()->GetFrequency();
 			}
-			//TreeNode *root = CreateTree(nodes);
+			cout << "EL TOTAL" << tot << endl;
+			TreeNode *root = CreateTree(nodes);
+			cout << "La Ruta de la RAIZ ES: " << root->GetData()->GetElement() << " su peso es" << root->GetData()->GetFrequency() << endl;
 
 			for (int i = 0; i < generalElements.size(); i++)
 			{
+
 				delete generalElements.at(i);
 				delete nodes.at(i);
 			}
-			cout<<"FINAL..."<<endl;
 
+			cout << "FINAL..." << endl;
 		}
 		else
 		{
@@ -213,36 +230,7 @@ int IndexOf(vector<TreeElement *> elements, string toCompare)
 	}
 	return -1;
 }
-/* 
- descripcion:
- Params: 
- Retorna: 
- Errores: */
-/*vector<TreeElement *> Ordenar(vector<TreeElement *> elements)
-{
-	TreeElement *temp;
-	vector<TreeElement *> tempElements;
-	for (int i = 1; i < elements.size(); i++)
-	{
-		for (int j = 0; j < elements.size() - 1; j++)
-		{
-			if (elements.at(j)->GetFrequency() > elements.at(j + 1)->GetFrequency())
-			{
-				temp = elements.at(j);
-				elements.at(j) = elements.at(j + 1);
-				elements.at(j + 1) = temp;
-			}
-		}
-	}
 
-	for (int i = elements.size() - 1; i >= 0; i--)
-	{
-		tempElements.push_back(elements.at(i));
-	}
-	elements = tempElements;
-	return elements;
-}
-*/
 /* 
  descripcion:
  Params: 
@@ -271,7 +259,11 @@ vector<TreeNode *> Ordenar(vector<TreeNode *> elements)
 	{
 		for (int j = 0; j < elements.size() - 1; j++)
 		{
-			if (elements.at(j)->GetData()->GetFrequency() > elements.at(j + 1)->GetData()->GetFrequency())
+			if (elements.at(j)->GetData()->GetFrequency() == elements.at(j + 1)->GetData()->GetFrequency())
+			{
+				//cout<<"IGUALES"<<endl;
+			}
+			else if (elements.at(j)->GetData()->GetFrequency() > elements.at(j + 1)->GetData()->GetFrequency())
 			{
 				temp = elements.at(j);
 				elements.at(j) = elements.at(j + 1);
@@ -279,11 +271,12 @@ vector<TreeNode *> Ordenar(vector<TreeNode *> elements)
 			}
 		}
 	}
-
 	for (int i = elements.size() - 1; i >= 0; i--)
 	{
 		tempElements.push_back(elements.at(i));
+		cout << i << "p: " << tempElements.at(tempElements.size() - 1)->GetData()->GetElement() << " - " << tempElements.at(tempElements.size() - 1)->GetData()->GetFrequency() << endl;
 	}
+
 	elements = tempElements;
 	return elements;
 }
@@ -294,11 +287,63 @@ vector<TreeNode *> Ordenar(vector<TreeNode *> elements)
  Errores: */
 TreeNode *CreateTree(vector<TreeNode *> Nodes)
 {
-	TreeNode *temp;
-	vector<TreeNode *> tempNodes=Nodes;
-	while (tempNodes.size() != 1)
+	vector<TreeNode *> tempNodes = Nodes;
+	int i = tempNodes.size();
+	while (tempNodes.size() > 1) //Rendimiento log n
 	{
-		Ordenar(tempNodes);
+		stringstream root;
+		vector<TreeNode *> temptempNodes;
+		TreeNode *child1 = tempNodes.at(tempNodes.size() - 1);
+		TreeNode *child2 = tempNodes.at(tempNodes.size() - 2);
+		size_t newFrequency = child1->GetData()->GetFrequency() + child2->GetData()->GetFrequency();
+		for (int i = 0; i < tempNodes.size() - 2; i++)
+		{
+			temptempNodes.push_back(tempNodes.at(i));
+		}
+		if (child1->GetData()->GetFrequency() <= child2->GetData()->GetFrequency())
+		{
+			root << child1->GetData()->GetElement() << child2->GetData()->GetElement();
+			TreeNode *Nodex = new TreeNode(*new TreeElement(root.str(), newFrequency));
+
+			Nodex->AddChild(child2);
+			Nodex->AddChild(child1);
+			temptempNodes = poner(temptempNodes, Nodex);
+		}
+		else
+		{ //child2<child1
+			root << child2->GetData()->GetElement() << child1->GetData()->GetElement();
+			TreeNode *Nodex = new TreeNode(*new TreeElement(root.str(), newFrequency));
+			Nodex->AddChild(child1);
+			Nodex->AddChild(child2);
+			temptempNodes = poner(temptempNodes, Nodex);
+		}
+
+		cout << "RECIEN CREADO: " << temptempNodes.at(temptempNodes.size() - 1)->GetData()->GetElement() << "peso: " << temptempNodes.at(temptempNodes.size() - 1)->GetData()->GetFrequency() << endl;
+		tempNodes = Ordenar(temptempNodes);
 	}
-	return temp;
+	return tempNodes.at(0);
+}
+
+vector<TreeNode *> poner(vector<TreeNode *> nodes, TreeNode *element)
+{
+	bool flag = false;
+	int x = 0;
+	for (int i = nodes.size() - 1; i > 0; i--)
+	{
+		if (nodes.at(i)->GetData()->GetFrequency() == element->GetData()->GetFrequency())
+		{
+			flag = true;
+			x = i;
+			break;
+		}
+	}
+	if (flag)
+	{
+		nodes.insert(nodes.begin() + (x - 1), element);
+	}
+	else
+	{
+		nodes.push_back(element);
+	}
+	return nodes;
 }
